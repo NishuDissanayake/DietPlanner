5a4c72c2c2dd64404e9d36d66be78581c72a010f
amasha26 <amashagunathilaka26@gmail.com>
2022-05-04 02:13:50 +0530

    navbar

--- healthy-ceylon/src/App.js
+++ healthy-ceylon/src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="APP">
+      <Navbar
+        bg="myRed"
+        variant="dark"
+        sticky="top"
+        expand="sm"
+        collapseOnSelect
+      >
+        <Navbar.Brand>
+          <img src="./images/logo.png" width="60px" height="60px"></img>
+        </Navbar.Brand>
+
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link href="Home">Home</Nav.Link>
+            <Nav.Link href="About">About</Nav.Link>
+            <Nav.Link href="Services">Services</Nav.Link>
+            <NavDropdown title="Pages">
+              <NavDropdown.Item href="Pages/Page1">Page 1</NavDropdown.Item>
+              <NavDropdown.Item href="Pages/Page2">Page 2</NavDropdown.Item>
+              <NavDropdown.Item href="Pages/Page3">Page 3</NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link href="Blog">Blog</Nav.Link>
+            <Nav.Link href="SignUp">SignUp</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
-
 export default App;
