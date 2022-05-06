6bcbae7272bfa9afb7e1097a96fc1fcd4f1eceac
NishuDissanayake <nishudissanayake5252@gmail.com>
2022-05-06 17:52:37 +0530

    folder structure

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+import GetStarted from './Pages/GetStarted/GetStarted';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Recipes from './Pages/Recipes/Recipes';
+import SignUp from './Pages/SignUp/SignUp';
+import AdminDashboard from './Pages/AdminDashboard/AdminDashboard';
+import Profile from './Pages/Profile/Profile';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+  <BrowserRouter>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/getstarted" element={<GetStarted />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<AdminDashboard />} />
+      </Routes>
+      <Footer />
+      </div>
+    </BrowserRouter> 
+  );
+}
+
+export default App;
